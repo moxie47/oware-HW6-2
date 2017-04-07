73497744a46ff3ca54bf82d6093e98c5bf6ff396
E V Level <evlevel@stthomas.edu>
2017-04-07 16:55:22 -0500

    Adding main startup message, describing HW-5 activities...

--- Oware/src/Oware.java
+++ Oware/src/Oware.java
@@ -4,7 +4,11 @@ public class Oware
 
 	public static void main(String[] args)
 	{
-		StdOut.println("Welcome to Warri47.");
+		StdOut.println("Welcome to Oware.");
+		StdOut.println("Each student will fork the starting");
+		StdOut.println("https://github.com/evlevel/oware repo");
+		StdOut.println("into their own GitHub clone...");
+		
 
 	}
 
