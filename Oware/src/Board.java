81760c1638104682b40ed73a648a9aaa8d9bae1a
Mike Pone <mike.a.pone@gmail.com>
2017-04-08 11:17:03 -0500

    added pits

--- Oware/src/Board.java
+++ Oware/src/Board.java
@@ -5,6 +5,19 @@ public class Board
 	
 	public Board(){
 		allPits = new Pit[12];
+		for (int i=0;i<12; i++) {
+			Pit newPit = new Pit(i, (int) i/6);
+			allPits[i] = newPit;
+		}
+
+		for (int i=0;i<12; i++) {
+			Pit pit = allPits[i];
+			Pit pitPrev = allPits[(12 + i-1)%12]; 
+			Pit pitNext = allPits[(i+1)%12];
+			pit.setNextPit(pitNext);
+			pit.setPrevPit(pitPrev);
+		}
+		
 	}
 
 	public static void main(String[] args)
