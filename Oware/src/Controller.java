500c4d36275bd4fea127954d7e3c939346968221
E V Level <evlevel@stthomas.edu>
2017-04-08 11:39:28 -0500

    First commit of Oware...

--- Oware/src/Controller.java
+++ Oware/src/Controller.java
@@ -5,6 +5,12 @@ public class Controller
 	private Player player0;
 	private Player player1;
 	private Player playerToMove;
+	private Board board;
+	
+	public Controller(Board b)
+	{
+		board = b;
+	}
 
 	public static void main(String[] args)
 	{
@@ -14,11 +20,30 @@ public class Controller
 
 	private Pit active;
 
-	public Move makeMoveForCupNumber(int pitNumber)
+	public boolean makeMoveStartingAt(int pitNumber)
 	{
-		active = Board.getPit(pitNumber);
+		active = board.getPit(pitNumber);
+		StdOut.println("Move starts at pit number " + pitNumber);
 
-		return new Move();
+		int numSeeds = active.getAllSeeds();
+		StdOut.println("Removed " + numSeeds + " seeds");
+		
+		StdOut.println("Sowing in ccw direction...");
+		
+		while (numSeeds > 0)
+		{
+			active = active.getNext();
+			StdOut.println("Processing next pit=> " + active.getPitNumber());
+			
+			active.sowOneSeed();
+			StdOut.println("Sowing one seed to it, giving its new count as: " + active.getNumSeeds());
+			
+			numSeeds--;
+		}
+		
+		StdOut.println("All seeds sown.");
+		
+		return true;
 	}
 
 }
