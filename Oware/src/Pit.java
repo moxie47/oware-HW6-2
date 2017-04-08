e3a1d24bc2cbf03f8d2225226ed94026d34c8a8a
Mike Pone <mike.a.pone@gmail.com>
2017-04-08 10:40:42 -0500

    implemented Pit

--- Oware/src/Pit.java
+++ Oware/src/Pit.java
@@ -2,10 +2,15 @@
 public class Pit
 {
 	private int numSeeds;
-
-	public Pit()
+	private final int ID;
+	private Pit nextPit;
+	private Pit prevPit;
+	private final int rowNum;
+	
+	public Pit(int ID, int rowNum)
 	{
-
+		this.ID=ID;
+		this.rowNum=rowNum;
 	}
 
 	public int getNumSeeds()
@@ -18,11 +23,35 @@ public class Pit
 		this.numSeeds = num;
 	}
 
+	public Pit getNextPit() {
+		return nextPit;
+	}
+
+	public void setNextPit(Pit nextPit) {
+		this.nextPit = nextPit;
+	}
+
+	public Pit getPrevPit() {
+		return prevPit;
+	}
+
+	public void setPrevPit(Pit prevPit) {
+		this.prevPit = prevPit;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public int getRowNum() {
+		return rowNum;
+	}
+
 	public static void main(String[] args)
 	{
 		// test the Pit...
 
-		Pit test = new Pit();
+		Pit test = new Pit(3,1);
 		test.setNumSeeds(4);
 
 	}
